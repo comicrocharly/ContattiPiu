1ccb7b574fcb8a4be6994611e535488e554d58d8
Carlo <Carlo@LAPTOP-K5JLIN0U>
2022-09-16 22:45:55 +0200

    Contatto Dao and getNome method

--- Codice/Java/src/main.java
+++ Codice/Java/src/main.java
@@ -1,6 +1,8 @@
 import java.sql.SQLException;
 
 import database.DatabaseConnect;
+import postgresDAO.*;
+
 
 public class main {
 
@@ -8,17 +10,23 @@ public class main {
 		// TODO Auto-generated method stub
 		
 		DatabaseConnect connect = null;
+		ContattoDAO contatto = null;
+		
 		try {
 			connect = new DatabaseConnect();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
+		contatto = new ContattoDAO();
 		
 		
 		System.out.println("Ciao " + connect.getUrl());
+		
+			System.out.println(contatto.getNome());
+		
+		
 		
 		
 	}
--- Codice/Java/src/postgresDAO/ContattoDAO.java
+++ Codice/Java/src/postgresDAO/ContattoDAO.java
@@ -7,7 +7,45 @@ import database.DatabaseConnect;
 
 public class ContattoDAO {
 	
-	private Contatto c;
 	private Connection link;
+	private Contatto c;
 
+	public ContattoDAO() {
+		
+		try {
+			link = DatabaseConnect.getInstance().getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Connection failed.");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String getNome() {
+		
+		PreparedStatement s;
+		String n = null;
+		
+		try {
+			s = link.prepareStatement(
+					"SELECT nome " 
+					+ "FROM contatto");
+			
+			ResultSet rs = s.executeQuery();
+			while(rs.next()) {
+				n += rs.getString("nome") + " ";
+			}
+			
+			rs.close();
+			return n;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
 }
+
