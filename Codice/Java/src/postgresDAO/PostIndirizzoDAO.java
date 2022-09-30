702dfe241b78710e0d5945bfd427c491625d9be9
Lino Strina <strinalino@yahoo.com>
2022-09-30 17:00:25 +0200

    Added getIndirizzi

--- Codice/Java/src/postgresDAO/PostIndirizzoDAO.java
+++ Codice/Java/src/postgresDAO/PostIndirizzoDAO.java
@@ -40,6 +40,7 @@ public class PostIndirizzoDAO {
 			while(rs.next()) {
 				indList.add(new Indirizzo(rs.getInt(0),rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4)));
 			}
+			rs.close();
 		}
 		catch(SQLException e) {
 		e.printStackTrace();
