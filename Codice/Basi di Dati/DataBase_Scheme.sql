62979586c7802f66e02b1be08a359d348bcfb980
Carlo Onofrio <52512202+comicrocharly@users.noreply.github.com>
2023-01-15 00:58:24 +0100

    Update DataBase_Scheme.sql
    Controllo_ordine_inserimento trigger removed

--- Codice/Basi di Dati/DataBase_Scheme.sql
+++ Codice/Basi di Dati/DataBase_Scheme.sql
@@ -229,48 +229,6 @@ CREATE OR REPLACE TRIGGER CONTROLLO_DELETE_TELEFONO AFTER DELETE ON RECAPITO
 FOR EACH ROW EXECUTE FUNCTION controllo_delete_telefono();
 
 
-CREATE OR REPLACE FUNCTION controllo_ordine_inserimento_recapiti() RETURNS TRIGGER AS $controllo_ordine_inserimento_recapiti$
-
-DECLARE
-
-Tipo_in Telefono.tipo%TYPE;
-cont INTEGER := 0;
-
-BEGIN
-
-SELECT tel.tipo INTO Tipo_in
-FROM Telefono AS tel
-WHERE NEW.numero = tel.numero AND NEW.prefisso = tel.prefisso;
-
-SELECT COUNT (Numero) INTO cont
-FROM Recapito
-WHERE Cont_ID = new.Cont_ID;
-
-IF(Cont = 0 AND Tipo_in = 'Mobile') THEN
-
-RAISE EXCEPTION 'Inserire prima un Fisso';
-
-END IF;
-
-IF (Cont = 1 AND Tipo_in = 'Fisso') THEN
-
-RAISE EXCEPTION 'Inserire un Mobile';
-
-END IF;
-
-
-RETURN NEW;
-
-END;
-
-$controllo_ordine_inserimento_recapiti$
-
-LANGUAGE plpgsql;
-
-CREATE TRIGGER CONTROLLO_ORDINE_INSERIMENTO_RECAPITI BEFORE INSERT OR UPDATE ON RECAPITO
-FOR EACH ROW EXECUTE FUNCTION controllo_ordine_inserimento_recapiti();
-
-
 CREATE OR REPLACE FUNCTION controllo_telefono_reindirizzato() RETURNS TRIGGER AS $controllo_telefono_reindirizzato$
 
 DECLARE
